5d33f9868a9398ba3086865fbb1a3125cea38a99
RomanLukach <wisefox17@gmail.com>
2022-10-23 22:55:15 +0300

    added hw5_2_rlukach_v01

---
+++ hw5_2_rlukach_v01.js
@@ -0,0 +1,18 @@
+// Пример кода с использованием динамической типизации в JS
+
+
+// variable "car" declared but undefined
+let car;
+console.log(typeof car);
+
+// variable "car" change data type to string
+car = 'Mercedes'; 
+console.log(typeof car);
+
+// variable "car" change data type to number
+car = 10;
+console.log(typeof car);
+
+// variable "car" change data type to bolean
+car =  true;
+console.log(typeof car);
